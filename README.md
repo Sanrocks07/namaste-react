4096aba8de75378e13f46c69fb46b0b917dbc768
Sanrocks07 <sanchitpillai@gmail.com>
2026-03-10 21:22:50 +0530

    feat: Add Redux Toolkit integration with cart slice and app store setup

--- README.md
+++ README.md
@@ -56,3 +56,12 @@ import {Component} from "path";
 -Client Side Routing
 -Server Side Routing
 
+
+#Redux Toolkit
+- Install @reduxjs/toolkit and react-redux
+- Build our store
+- Connect our store
+- Slice (cartSlice)
+- dispatch(action)
+- Selector
+
--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Contact from "./components/Contact";
 import Error from "./components/Error";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import RestaurantMenu from "./components/RestaurantMenu";
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore";
 //import Grocery from "./components/Grocery";
 
 //Lazy load Grocery component
@@ -17,12 +19,13 @@ import RestaurantMenu from "./components/RestaurantMenu";
 const Grocery = lazy(() => import("./components/Grocery"));
 
 const AppLayout = () => {
-
   return (
-    <div className="app">
-      <Header />
-      <Outlet />
-    </div>
+    <Provider store={appStore}>
+      <div className="app">
+        <Header />
+        <Outlet />
+      </div>
+    </Provider>
   );
 };
 
