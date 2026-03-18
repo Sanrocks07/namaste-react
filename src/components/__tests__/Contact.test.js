bef6c747c98e5b2f73bf7541115cfd4a3bc5299d
Sanrocks07 <sanchitpillai@gmail.com>
2026-03-18 20:43:06 +0530

    fix: replace test with it in Contact component tests for consistency

--- src/components/__tests__/Contact.test.js
+++ src/components/__tests__/Contact.test.js
@@ -2,7 +2,7 @@ import { render, screen } from "@testing-library/react";
 import Contact from "../Contact";
 import "@testing-library/jest-dom";
 
-test("should load contact us page", () => {
+it("should load contact us page", () => {
   render(<Contact />);
 
   const heading = screen.getByRole("heading");
@@ -11,7 +11,7 @@ test("should load contact us page", () => {
   expect(heading).toBeInTheDocument();
 });
 
-test("should load button inside Contact component", () => {
+it("should load button inside Contact component", () => {
   render(<Contact />);
 
   const button = screen.getByText("Submit");
